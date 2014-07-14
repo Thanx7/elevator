589d14555564c13fc0990f8366affb2d66942900
Thanx7 <Aliaksandr_Doubeikovsky@epam.com>
2014-07-14 16:11:50 +0300

    Elevator task after refactoring / v 1.0

--- src/main/java/by/gsu/epamlab/Controller.java
+++ src/main/java/by/gsu/epamlab/Controller.java
@@ -28,7 +28,6 @@ import org.apache.log4j.Logger;
 import by.gsu.epamlab.beans.Passenger;
 
 public class Controller extends JPanel {
-	static int counterTmp = 0;
 	private static final long serialVersionUID = 1L;
 	private static final Logger LOGGER = Logger.getLogger(Controller.class);
 
